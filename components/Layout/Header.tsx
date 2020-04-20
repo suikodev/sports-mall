5673a03fe78692eab20205ccc3f37c42b6262720
Nacht <realnacht@outlook.com>
2020-04-21 01:55:39 +0800

    refactor(header): add label for input

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -26,7 +26,9 @@ export const Header = () => {
 
           <FlexNavItem flex="1 1 auto">
             <InputGroup flex="0 1 40rem">
+              <label htmlFor="product-search-bar" aria-label="商品搜索"></label>
               <Input
+                id="product-search-bar"
                 onBlur={() => setShow(true)}
                 onFocus={() => setShow(false)}
                 placeholder="搜索商品..."
